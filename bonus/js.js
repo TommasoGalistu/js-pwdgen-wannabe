d7d9ed6bfa14feb4191d21c681fb818d5269a142
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-03 16:45:57 +0200

    creazione funzione di base

--- bonus/js.js
+++ bonus/js.js
@@ -1,1 +1,11 @@
-let testoDaInput = document.getElementById('dettagliInput')
+// bottone input
+let buttonSub = document.getElementById('buttonSubmit');
+
+// testo da cambiare
+let testoDaInput = document.getElementById('dettagliInput');
+
+
+buttonSub.addEventListener('click', (event) => {
+    event.preventDefault()
+    console.log('dentro')
+})
