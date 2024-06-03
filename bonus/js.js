25be8bfb643076c59242783cd01519f7e5c19ba8
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-03 16:57:03 +0200

    inserimento funzione senza controlli

--- bonus/js.js
+++ bonus/js.js
@@ -1,11 +1,18 @@
 // bottone input
 let buttonSub = document.getElementById('buttonSubmit');
-
+// input
+let contNome = document.getElementById('validationCustom01').value;
+let contCognome = document.getElementById('validationCustom02').value;
+let contCittà = document.getElementById('validationCustom03').value;
+let contZipCode = document.getElementById('validationCustom05').value;
 // testo da cambiare
 let testoDaInput = document.getElementById('dettagliInput');
-
+let nome, cognome, citta, codPost;
 
 buttonSub.addEventListener('click', (event) => {
     event.preventDefault()
-    console.log('dentro')
+    let testoCompleto = `Ti chiami ${contNome} e il tuo cognome è ${contCognome}, abiti a ${contCittà} zip code:${contZipCode} `
+    testoDaInput.textContent = testoCompleto
+
+    
 })
