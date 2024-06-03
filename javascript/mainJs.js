29d4955defb9075a57375635f233fbc5ea040f30
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-03 14:47:53 +0200

    fine lavoro

--- javascript/mainJs.js
+++ javascript/mainJs.js
@@ -1,17 +1,20 @@
 // inizio variabili
 // creazione variabile di h2 dal dom
-let testoVisibileUtente = document.getElementById('password')
+let testoVisibileUtente = document.getElementById('password');
 
 // variabili input
 // variabile del nome inserito dall'imput dell'utente
-let nome = prompt('scrivi il tuo nome')
+let nome = prompt('scrivi il tuo nome');
 // variabile del cognome inserito dall'input dell'utente
-let cognome = prompt('scrivi il tuo cognome')
+let cognome = prompt('scrivi il tuo cognome');
 // variabile del colore inserito da input utente
-let colorePref = prompt("Qual'è il tuo colore preferito?")
+let colorePref = prompt("Qual'è il tuo colore preferito?");
 // variabile di numero casuale da inserire alla password
-let numeroCasuale = Math.floor(Math.random() * 23)
+let numeroCasuale = Math.floor(Math.random() * 23);
 
 
 // inserimento effettivo del testo 
-testoVisibileUtente.innerHTML = nome + cognome + colorePref + numeroCasuale
+let testoCompleto = `La tua password è: ${nome}${cognome}${colorePref}${numeroCasuale}`;
+testoVisibileUtente.innerHTML = testoCompleto;
+
+
